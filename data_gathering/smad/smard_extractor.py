b30bcf2054bdf9e3020010fcd8dde12238169ae8
kyrbii <149487139+kyrbii@users.noreply.github.com>
2023-12-29 13:01:18 +0100

    created smard_extractor

---
+++ data_gathering/smad/smard_extractor.py
@@ -0,0 +1,11 @@
+def startup():
+    # startDate = '20220101' alias
+    # endDate = '20221231' alias
+    # Sommerzeit (GMT+2) 2022 in Deutschland Von Sonntag, 27. März 02:00 Uhr bis Sonntag, 30. Oktober  03:00 Uhr
+    #
+    #
+    pass
+
+
+if __name__ == '__main__':
+    startup()
