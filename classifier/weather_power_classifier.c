373a376b65c73a7a596eac5e66eb5ff139184ba4
Korbinian Eller <korbinian@eller-software.de>
2024-01-05 19:07:13 +0100

    Idea finding Co-authored-by: KayJitsu

---
+++ classifier/weather_power_classifier.c
@@ -0,0 +1,51 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <math.h>
+
+/*
+
+
+func csv-reader bauen -> überlegen, machen wir die ganze csv komplett, oder in Stuecken(Haelfte oder so), weil ultra riesig
+
+
+func csv-ausgeben fuer testen
+
+
+func iteriere durch alle Datensaetze durch und test/classify
+
+    func trainingsdatensatz erstellen -> Volles File - (minus) Testdatensatz
+
+    func classifier
+        
+
+        
+    func validator
+
+        Ueberlegen. Implementieren wir eine Kulanz des validators, sodass wir Werte auf/abrunden bzw. eine range, also innerhalb von 5% oder so -> beim auf/abrunden kanns pasieren, dass der Wert weiter wegrutscht dur auf/abrundung
+
+
+func Fehlerueberpruefung in der Eingabe
+
+    func ueberpruefen auf Fehler und zuweisen an die variablen
+
+    Eingabe: .\classifier.exe n dist .\weather.csv d(x) .\power.csv d(y)
+    argv[0] = "classifier.exe"
+    argv[1] = "rows"
+    argv[2] = "value for p in the minkowski distance"
+    argv[3] = "x-filename"
+    argv[4] = "dimensions of x"
+    argv[5] = "y-filename"
+    argv[6] = "dimensions of y"
+
+
+func main
+
+*/
+
+int main(int argc, char const* argv[]) {    //arc: argument count   argv: argument vector
+    /* code */
+    return 0;
+}
